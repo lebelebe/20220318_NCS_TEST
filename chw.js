b4a01f7401303d6e6639e7583564e6160df2e477
leblebe <jhw9627@gmail.com>
2022-03-18 11:13:29 +0900

    노드라우팅완료

--- chw.js
+++ chw.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
+var postsend = require('./routes/postsend');
+var getsend = require('./routes/getsend');
+var sqlsend = require('./routes/sqlsend');
+
 
 app.get('/', (req, res) => {
     res.send('첫페이지 출력')
 })
+
+app.use('/postsend', postsend);
+app.use('/getsend', getsend);
+app.use('/sqlsend', sqlsend);
 
 
 app.listen(8080, () => {
