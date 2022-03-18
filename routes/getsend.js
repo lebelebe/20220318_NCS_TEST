33dffdf5c23ea74a3eca53f74ccc41615f1859c6
leblebe <jhw9627@gmail.com>
2022-03-18 12:25:11 +0900

    출력준비완료

--- routes/getsend.js
+++ routes/getsend.js
@@ -1,8 +1,30 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
+var dbconfig = require('../db/config.js');
+const connection = mysql.createConnection(dbconfig);
 
 router.get('/', (req, res) => {
     res.send('/getsend 요청 응답')
+})
+router.get('/getjson', (req, res) => {
+    res.send({'url':'/getsend/getjson'})
+})
+router.post('/post', (req, res) => {
+    res.send({
+        'url':'/getsend/post',
+        'title':'getsend.js내의 post라우팅'
+    });
+})
+router.get('/awssql', (req, res) => {
+    connection.query(
+        'SELECT * FROM interview;',
+        (error, rows) => {
+            if (error) throw error;
+            console.log('해당스키마안의 테이블내용: ', rows);
+            res.send(rows);
+        }
+    )
 })
 
 module.exports = router;
--- routes/sqlsend.js
+++ routes/sqlsend.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
 
 
 module.exports = router;
