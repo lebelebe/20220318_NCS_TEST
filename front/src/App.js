3e875c991e2879f98e4fd6cd95a61131d11b6854
leblebe <jhw9627@gmail.com>
2022-04-11 09:58:00 +0900

    풀페이지세팅

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,20 @@
 import Preinterview from './component/Preinterview';
 import Classcom from './component/Classcomponent';
+import { FullPage, Slide } from 'react-full-page';
+import Fullpage1 from './component/Fullpage1'
 
 function App() {
   return (
     <div className="Wrap">
-      <Preinterview type='aws'></Preinterview>
+      <FullPage>
+        <Slide>
+          <Fullpage1></Fullpage1>
+        </Slide>
+        <Slide>
+          <h1>Another slide content</h1>
+        </Slide>
+      </FullPage>
+      {/* <Preinterview type='aws'></Preinterview> */}
     </div>
   );
 }
---
+++ front/src/component/Fullpage1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Fullpage1 extends Component {
+    render() {
+        return (
+            <div id='page1'>
+                <h1>컴포넌트 넣어보기</h1>
+            </div>
+        );
+    }
+}
+
+export default Fullpage1;
