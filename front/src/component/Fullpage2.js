c3993f8286add84ab364c4d406ce78d0959993ee
leblebe <jhw9627@gmail.com>
2022-04-15 18:30:59 +0900

    fullpage2제작중

---
+++ front/src/component/Fullpage2.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {Card, CardBody, CardImg, CardTitle, Button, CardGroup} from 'reactstrap'
+import './css/card.scss'
+
+const Fullpage2 = (props) =>{
+    return(
+        <div id='page2'>
+            <div>
+                <h2>
+                    기획 및 디자인
+                </h2>
+                <p>
+                    UX를 고려하여 UI를 구현하려고 노력하였고 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Fullpage2;
